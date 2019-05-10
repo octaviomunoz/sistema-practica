90a429e3b3f3524f81d543802b28a682b96362fd
Octavio <ostavio09@gmail.com>
2019-05-10 11:45:12 -0300

    Rectificacion en los datos del model EvalucionPractica

--- practica_spring/src/main/java/com/practica/model/EvaluacionPractica.java
+++ practica_spring/src/main/java/com/practica/model/EvaluacionPractica.java
@@ -17,10 +17,49 @@ public class EvaluacionPractica {
 	private Date fecha;
 
 	@NotNull
-	private int[] tabla1 = new int[10];
+	private int tabla1_pregunata1;
 
 	@NotNull
-	private int[] tabla2 = new int[5];
+	private int tabla1_pregunata2;
+
+	@NotNull
+	private int tabla1_pregunata3;
+
+	@NotNull
+	private int tabla1_pregunata4;
+
+	@NotNull
+	private int tabla1_pregunata5;
+
+	@NotNull
+	private int tabla1_pregunata6;
+
+	@NotNull
+	private int tabla1_pregunata7;
+
+	@NotNull
+	private int tabla1_pregunata8;
+
+	@NotNull
+	private int tabla1_pregunata9;
+
+	@NotNull
+	private int tabla1_pregunata0;
+
+	@NotNull
+	private int tabla2_pregunata1;
+
+	@NotNull
+	private int tabla2_pregunata2;
+
+	@NotNull
+	private int tabla2_pregunata3;
+
+	@NotNull
+	private int tabla2_pregunata4;
+
+	@NotNull
+	private int tabla2_pregunata5;
 
 	@NotNull
 	@Size(min = 1, max = 200)
@@ -33,6 +72,8 @@ public class EvaluacionPractica {
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "id_practica")
 	private Practica practica;
+
+
 
 
 
@@ -70,35 +111,243 @@ public class EvaluacionPractica {
 	}
 
 	/**
-	* Returns value of t
+	* Returns value of tabla1_pregunata1
 	* @return
 	*/
-	public int[] getTabla1() {
-		return tabla1;
+	public int getTabla1_pregunata1() {
+		return tabla1_pregunata1;
 	}
 
 	/**
-	* Sets new value of t
+	* Sets new value of tabla1_pregunata1
 	* @param
 	*/
-	public void setTabla1(int[] tabla1) {
-		this.tabla1 = tabla1;
+	public void setTabla1_pregunata1(int tabla1_pregunata1) {
+		this.tabla1_pregunata1 = tabla1_pregunata1;
 	}
 
 	/**
-	* Returns value of t
+	* Returns value of tabla1_pregunata2
 	* @return
 	*/
-	public int[] getTabla2() {
-		return tabla2;
+	public int getTabla1_pregunata2() {
+		return tabla1_pregunata2;
 	}
 
 	/**
-	* Sets new value of t
+	* Sets new value of tabla1_pregunata2
 	* @param
 	*/
-	public void setTabla2(int[] tabla2) {
-		this.tabla2 = tabla2;
+	public void setTabla1_pregunata2(int tabla1_pregunata2) {
+		this.tabla1_pregunata2 = tabla1_pregunata2;
+	}
+
+	/**
+	* Returns value of tabla1_pregunata3
+	* @return
+	*/
+	public int getTabla1_pregunata3() {
+		return tabla1_pregunata3;
+	}
+
+	/**
+	* Sets new value of tabla1_pregunata3
+	* @param
+	*/
+	public void setTabla1_pregunata3(int tabla1_pregunata3) {
+		this.tabla1_pregunata3 = tabla1_pregunata3;
+	}
+
+	/**
+	* Returns value of tabla1_pregunata4
+	* @return
+	*/
+	public int getTabla1_pregunata4() {
+		return tabla1_pregunata4;
+	}
+
+	/**
+	* Sets new value of tabla1_pregunata4
+	* @param
+	*/
+	public void setTabla1_pregunata4(int tabla1_pregunata4) {
+		this.tabla1_pregunata4 = tabla1_pregunata4;
+	}
+
+	/**
+	* Returns value of tabla1_pregunata5
+	* @return
+	*/
+	public int getTabla1_pregunata5() {
+		return tabla1_pregunata5;
+	}
+
+	/**
+	* Sets new value of tabla1_pregunata5
+	* @param
+	*/
+	public void setTabla1_pregunata5(int tabla1_pregunata5) {
+		this.tabla1_pregunata5 = tabla1_pregunata5;
+	}
+
+	/**
+	* Returns value of tabla1_pregunata6
+	* @return
+	*/
+	public int getTabla1_pregunata6() {
+		return tabla1_pregunata6;
+	}
+
+	/**
+	* Sets new value of tabla1_pregunata6
+	* @param
+	*/
+	public void setTabla1_pregunata6(int tabla1_pregunata6) {
+		this.tabla1_pregunata6 = tabla1_pregunata6;
+	}
+
+	/**
+	* Returns value of tabla1_pregunata7
+	* @return
+	*/
+	public int getTabla1_pregunata7() {
+		return tabla1_pregunata7;
+	}
+
+	/**
+	* Sets new value of tabla1_pregunata7
+	* @param
+	*/
+	public void setTabla1_pregunata7(int tabla1_pregunata7) {
+		this.tabla1_pregunata7 = tabla1_pregunata7;
+	}
+
+	/**
+	* Returns value of tabla1_pregunata8
+	* @return
+	*/
+	public int getTabla1_pregunata8() {
+		return tabla1_pregunata8;
+	}
+
+	/**
+	* Sets new value of tabla1_pregunata8
+	* @param
+	*/
+	public void setTabla1_pregunata8(int tabla1_pregunata8) {
+		this.tabla1_pregunata8 = tabla1_pregunata8;
+	}
+
+	/**
+	* Returns value of tabla1_pregunata9
+	* @return
+	*/
+	public int getTabla1_pregunata9() {
+		return tabla1_pregunata9;
+	}
+
+	/**
+	* Sets new value of tabla1_pregunata9
+	* @param
+	*/
+	public void setTabla1_pregunata9(int tabla1_pregunata9) {
+		this.tabla1_pregunata9 = tabla1_pregunata9;
+	}
+
+	/**
+	* Returns value of tabla1_pregunata0
+	* @return
+	*/
+	public int getTabla1_pregunata0() {
+		return tabla1_pregunata0;
+	}
+
+	/**
+	* Sets new value of tabla1_pregunata0
+	* @param
+	*/
+	public void setTabla1_pregunata0(int tabla1_pregunata0) {
+		this.tabla1_pregunata0 = tabla1_pregunata0;
+	}
+
+	/**
+	* Returns value of tabla2_pregunata1
+	* @return
+	*/
+	public int getTabla2_pregunata1() {
+		return tabla2_pregunata1;
+	}
+
+	/**
+	* Sets new value of tabla2_pregunata1
+	* @param
+	*/
+	public void setTabla2_pregunata1(int tabla2_pregunata1) {
+		this.tabla2_pregunata1 = tabla2_pregunata1;
+	}
+
+	/**
+	* Returns value of tabla2_pregunata2
+	* @return
+	*/
+	public int getTabla2_pregunata2() {
+		return tabla2_pregunata2;
+	}
+
+	/**
+	* Sets new value of tabla2_pregunata2
+	* @param
+	*/
+	public void setTabla2_pregunata2(int tabla2_pregunata2) {
+		this.tabla2_pregunata2 = tabla2_pregunata2;
+	}
+
+	/**
+	* Returns value of tabla2_pregunata3
+	* @return
+	*/
+	public int getTabla2_pregunata3() {
+		return tabla2_pregunata3;
+	}
+
+	/**
+	* Sets new value of tabla2_pregunata3
+	* @param
+	*/
+	public void setTabla2_pregunata3(int tabla2_pregunata3) {
+		this.tabla2_pregunata3 = tabla2_pregunata3;
+	}
+
+	/**
+	* Returns value of tabla2_pregunata4
+	* @return
+	*/
+	public int getTabla2_pregunata4() {
+		return tabla2_pregunata4;
+	}
+
+	/**
+	* Sets new value of tabla2_pregunata4
+	* @param
+	*/
+	public void setTabla2_pregunata4(int tabla2_pregunata4) {
+		this.tabla2_pregunata4 = tabla2_pregunata4;
+	}
+
+	/**
+	* Returns value of tabla2_pregunata5
+	* @return
+	*/
+	public int getTabla2_pregunata5() {
+		return tabla2_pregunata5;
+	}
+
+	/**
+	* Sets new value of tabla2_pregunata5
+	* @param
+	*/
+	public void setTabla2_pregunata5(int tabla2_pregunata5) {
+		this.tabla2_pregunata5 = tabla2_pregunata5;
 	}
 
 	/**
