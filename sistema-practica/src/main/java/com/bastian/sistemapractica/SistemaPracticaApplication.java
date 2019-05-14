eb1d18b39e0c7c82d8b9f2fb3fe650fc97ef1089
Bastian <Bastian@192.168.11.4>
2019-05-13 20:11:04 -0400

    probando 123 xd

---
+++ sistema-practica/src/main/java/com/bastian/sistemapractica/SistemaPracticaApplication.java
@@ -0,0 +1,13 @@
+package com.bastian.sistemapractica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPracticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPracticaApplication.class, args);
+	}
+
+}
