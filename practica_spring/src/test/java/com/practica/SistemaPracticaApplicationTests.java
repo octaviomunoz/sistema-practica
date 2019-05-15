70bfa782c5879d6b882fa3e04818820740a0d352
Bastian <Bastian@192.168.11.4>
2019-05-15 18:12:00 -0400

    Creo que volvi en el tiempo

--- practica_spring/src/test/java/com/practica/SistemaPracticaApplicationTests.java
+++ practica_spring/src/test/java/com/practica/SistemaPracticaApplicationTests.java
@@ -1,31 +1,16 @@
 package com.practica;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.practica.model.Usuario;
-import com.practica.repo.UsuarioRepo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SistemaPracticaApplicationTests {
 
-	@Autowired
-	private UsuarioRepo repo;
-	
 	@Test
-	public void crearUsuarioTest() {
-		Usuario us = new Usuario();
-		us.setId(1);
-		us.setNombre("holaa");
-		us.setClave("holamundo");
-		Usuario retorno = repo.save(us);
-		assertTrue(retorno.getClave().equalsIgnoreCase(us.getClave()));
+	public void contextLoads() {
 	}
 
 }
