c8b72406e85f140cc974c768791d6c4e5e3d52bd
Bastian <Bastian@192.168.11.4>
2019-05-19 02:12:03 -0400

    Mejorada la vista de listardocente, incluye boton para editar y eliminar

--- practica_spring/src/main/java/com/practica/model/DocenteCrud.java
+++ practica_spring/src/main/java/com/practica/model/DocenteCrud.java
@@ -1,8 +1,18 @@
 package com.practica.model;
 
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DocenteCrud extends JpaRepository<Docente, Long> {
+	default Docente findOne(long id) { 
+        return (Docente) findById(id).orElse(null); 
+    }
+
+	
+
+
 
 }
