10f85ba472c51195569d595145a835a2c270c0fa
Bastian <Bastian@192.168.11.4>
2019-06-30 19:37:52 -0400

    Agregue un dato para probar

--- practica_spring/src/main/resources/data.sql
+++ practica_spring/src/main/resources/data.sql
@@ -142,6 +142,7 @@ INSERT INTO evaluadores(nombre, email, telefono, id_usuario) VALUES
 
 INSERT INTO practicas(actividades_realizar_practica, fecha_practica, id_alumno, id_empresa, id_evaluador, id_docente) VALUES
 ('crear','2019-07-22',1, 1, 1, 1),
+('crear','2019-07-22',1, 1, 1, 1),
 ('crear','2019-08-12',1, 1, 1, 2);
 
 INSERT INTO evaluacionpracticas(fecha,pregunta1,pregunta2,tabla1_pregunata1,tabla1_pregunata2,tabla2_pregunata1,tabla2_pregunata2) VALUES
